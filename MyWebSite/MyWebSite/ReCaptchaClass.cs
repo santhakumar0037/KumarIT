6480487bbc083cb75ae66e55db18b5d93ee44b8c
Santha kumar <santhakumar0037@gmail.com>
2018-03-02 11:01:38 +1000

    Sections updated with min-height

---
+++ MyWebSite/MyWebSite/ReCaptchaClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace MyWebSite
+{
+    public class ReCaptchaClass
+    {
+        public static string Validate(string EncodedResponse)
+        {
+            var client = new System.Net.WebClient();
+
+            string PrivateKey = "6LfHdkkUAAAAACCi22dh7XgO_CuoW3x3msUPhoG9";
+
+            var GoogleReply = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", PrivateKey, EncodedResponse));
+
+            var captchaResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<ReCaptchaClass>(GoogleReply);
+
+            return captchaResponse.Success;
+        }
+
+        [JsonProperty("success")]
+        public string Success
+        {
+            get { return m_Success; }
+            set { m_Success = value; }
+        }
+
+        private string m_Success;
+        [JsonProperty("error-codes")]
+        public List<string> ErrorCodes
+        {
+            get { return m_ErrorCodes; }
+            set { m_ErrorCodes = value; }
+        }
+
+
+        private List<string> m_ErrorCodes;
+    }
+}
---
+++ MyWebSite/MyWebSite/obj/Release/Package/PackageTmp/Content/CustomScript.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#arrow').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#Experties-section').offset().top
+        }, 500, 'linear');
+    });
+
+    $('#lblStatus').fadeOut(5000);
+
+    $('#ContactForm').click('hide');
+
+});
